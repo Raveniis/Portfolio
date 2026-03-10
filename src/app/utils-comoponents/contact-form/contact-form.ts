8e0f1f0914b9d927bdf8ee3efe06948093d8d903
Ven <128274869+Raveniis@users.noreply.github.com>
2026-03-10 15:42:58 +0800

    contact form – skeleton structure

--- src/app/utils-comoponents/contact-form/contact-form.ts
+++ src/app/utils-comoponents/contact-form/contact-form.ts
@@ -1,23 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { MaterialModules } from '../../../modules/module';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RecaptchaModule } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [MaterialModules, ReactiveFormsModule],
+  imports: [MaterialModules, ReactiveFormsModule, RecaptchaModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
   fb = inject(FormBuilder);
   emailForm!: FormGroup;
+  theme: ReCaptchaV2.Theme = 'light';
 
   constructor() {
+    const storedTheme = sessionStorage.getItem('theme');
+    this.theme = storedTheme ? (storedTheme as ReCaptchaV2.Theme) : 'light';
+
     this.emailForm = this.fb.group({
       name: ['', [Validators.max(64)]],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required, Validators.maxLength(256)]],
       recaptcha: ['', [Validators.required]],
     });
+  }
+
+  resolved(captchaResponse: any) {
+    console.log(`Resolved captcha with response: ${captchaResponse}`);
+  }
+
+  onSubmit() {
+    const payload = this.emailForm.value;
+    console.log(payload);
   }
 }
